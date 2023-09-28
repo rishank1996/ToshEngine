3f87a50255c8c60d075e47e083e3999c851b54e7
Rishank Kumar <rishank.kumar@gmail.com>
2023-09-28 21:08:13 +0530

    Add project files.

---
+++ Sandbox/src/main.cpp
@@ -0,0 +1,6 @@
+#include <engine.h>
+
+int main()
+{
+	Tosh::Hello();
+}
---
+++ ToshEngine/src/engine.cpp
@@ -0,0 +1,11 @@
+#include "engine.h"
+
+#include <iostream>
+
+namespace Tosh
+{
+	void Hello()
+	{
+		std::cout << "Welcome to Tosh Engine" << std::endl;
+	}
+}
